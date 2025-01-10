802188491c915bc4f59e586e36a7c2b5843c91a0
chuck21619 <charles.ward.johnston@gmail.com>
2025-01-10 11:11:20 -0500

    add excel parsing

--- main.js
+++ main.js
@@ -16,17 +16,45 @@ function dropHandler(ev) {
             if (item.kind === "file") {
                 const file = item.getAsFile();
                 console.log(`… file[${i}].name = ${file.name}`);
+                ExcelToJSON(file);
             }
         });
     } else {
         // Use DataTransfer interface to access the file(s)
         [...ev.dataTransfer.files].forEach((file, i) => {
             console.log(`… file[${i}].name = ${file.name}`);
+            ExcelToJSON(file);
         });
     }
 }
 
 function dragOverHandler(ev) {
-    console.log("File(s) in drop zone");
     ev.preventDefault();
+}
+
+function ExcelToJSON(file) {
+    console.log("ExcelToJSON");
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        var data = e.target.result;
+        var workbook = XLSX.read(data, {
+            type: 'binary'
+        });
+
+        workbook.SheetNames.forEach(function (sheetName) {
+            // Here is your object
+            var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+            var json_object = JSON.stringify(XL_row_object);
+            console.log(json_object);
+
+        })
+
+    };
+
+    reader.onerror = function (ex) {
+        console.log(ex);
+    };
+
+    reader.readAsBinaryString(file);
 }
